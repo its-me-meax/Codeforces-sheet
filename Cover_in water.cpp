4adeacc00e38a149a033c34976e5d03aaa956fa6
its-me-meax <ultragaming123q@gmail.com>
2026-01-09 11:57:38 +0530

    again...

--- Cover_in water.cpp
+++ Cover_in water.cpp
@@ -1,33 +1,38 @@
-class Solution {
-public:
-    void solve() {
-        int t;
-        cin >> t;
-        while (t--) {
-            int n;
-            cin >> n;
-            string s;
-            cin >> s;
+#include <bits/stdc++.h>
+using namespace std;
 
-            int ans = 0;
-            for (int i = 0; i < n; ) {
-                if (s[i] == '#') {
-                    i++;
-                    continue;
-                }
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
 
-                int j = i;
-                while (j < n && s[j] == '.') j++;
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        string s;
+        cin >> s;
 
-                int len = j - i;
-                if (len == 1) ans += 1;
-                else if (len == 2) ans += 2;
-                else ans += 2;
+        int ans = 0;
 
-                i = j;
+        for (int i = 0; i < n; ) {
+            if (s[i] == '#') {
+                i++;
+                continue;
             }
 
-            cout << ans << '\n';
+            int j = i;
+            while (j < n && s[j] == '.') j++;
+
+            int len = j - i;
+            if (len == 1) ans += 1;
+            else if (len == 2) ans += 2;
+            else ans += 2;
+
+            i = j;
         }
+
+        cout << ans << '\n';
     }
-};
+    return 0;
+}
